6e91756509fcf06a5e9f44511c70c1989daf3f6f
haytham taha alsharany <talsharany@gmail.com>
2020-08-27 16:18:29 +0300

    when expration

--- src/Game.kt
+++ src/Game.kt
@@ -1,13 +1,32 @@
 import kotlin.reflect.jvm.internal.impl.serialization.deserialization.DeserializationConfiguration
 
 fun main() {
-    var healthPoint: Int = 100
+    var healthPoint = 100
     val playerName: String = "Madrigal"
-    var isBlessed: Boolean = true
-    var playerStatus: String = "";
+    var isBlessed = true
+
     var auraColore= "none"
     var auraVisible:Boolean
     val karama:Int;
+
+    if(healthPoint == 100){
+        println(playerName+" is an excelant  condition!")
+    }else if(healthPoint in 90..99)
+        println(playerName+" has a few scraches  ")
+    else if(healthPoint in 75..89) {
+        if(isBlessed)
+            println(playerName + " has some minor wounds but is healing quite ! ")
+        else
+            println(playerName + " has some minor wounds  ")
+    }
+    else if(healthPoint in 15..74)
+        println(playerName+" looks prety hurt  ")
+    else if(healthPoint in 0..14)
+        println(playerName+" is in awful condition! ")
+    else
+        println("some thing wrong  ")
+
+
 
 karama = (Math.pow(Math.random(), (110 - healthPoint) / 100.0) * 20).toInt()
     auraVisible= isBlessed&&healthPoint>50;
@@ -21,7 +40,7 @@ karama = (Math.pow(Math.random(), (110 - healthPoint) / 100.0) * 20).toInt()
     }
     if(auraVisible&&auraColore!="none"&& isBlessed)
     {
-        playerStatus=auraColore;
+        println(" the new player\' status is :  $auraColore ")
     }
 
 
